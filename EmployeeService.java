536261242be464530b33eecc30e2b531183bc35d
Satish Gampala <sadduri@uscold.com>
2026-06-25 20:20:26 +0530

    adding Employee

---
+++ EmployeeService.java
@@ -0,0 +1,9 @@
+
+public class EmployeeService {
+
+    public void addEmployee(Employee employee) {
+        System.out.println("Employee Added Successfully");
+        employee.display();
+    }
+}
+
